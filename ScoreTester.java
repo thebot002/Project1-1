211643a4abfc79cbfb1aceb1e6e938c02ef1f186
WizardCream <32612120+WizardCream@users.noreply.github.com>
2017-11-21 14:22:15 +0100

    Add files via upload

---
+++ ScoreTester.java
@@ -0,0 +1,17 @@
+//package ScoreTracker;
+
+public class ScoreTester{
+	public static void main(String[] args){
+		HighScoreManager hm = new HighScoreManager();
+		hm.addScore("a", 100);
+		hm.addScore("b", 200);
+		hm.addScore("c", 110);
+		hm.addScore("d", 190);
+		hm.addScore("e", 120);
+		hm.addScore("f", 180);
+		hm.addScore("g", 130);
+		hm.addScore("h", 170);
+		hm.addScore("i", 140);
+		hm.addScore("j", 150);
+	}
+}
