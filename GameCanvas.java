f68e56ea7164e16f12aac1deaa8882249d58b0e8
nicxcin <nicholas.beaumont21@gmail.com>
2017-11-24 14:30:16 +0100

    Revert "Time box now functions"
    This reverts commit 91375b9877759229a32bf8f9034679f0c70b00fe.

--- GameCanvas.java
+++ GameCanvas.java
@@ -13,13 +13,11 @@ class GameCanvas extends PentPanel implements ActionListener {
 	public static void main(String[] args) {}
 
     private ShapeBox shapeBox;
-    private TextBox timeBox;
     private ShapeList shapeList;
     private Timer timer;
     private Shape nextShape;
     private ArrayList<ScoreBox> scoreBoxes= new ArrayList<ScoreBox>();
     private Random random = new Random();
-    private int time = 0;
 
     public int[][] board = {
         {0,0,0,0,0},
@@ -55,8 +53,7 @@ class GameCanvas extends PentPanel implements ActionListener {
 
     private void tick() {
         drawScoreBoxes();
-        time++;
-        timeBox.setTarget(time);
+
         int rint = random.nextInt(12);
 
         nextShape = shapeList.get(rint);
@@ -123,7 +120,7 @@ class GameCanvas extends PentPanel implements ActionListener {
         TextBox scoreBox = new TextBox(SQ/2, SQ*4, font, SQ, "Score");
         TextBox highScoreBox = new TextBox((SQ*23)/2, SQ*6, font, SQ, "High Score");
         TextBox levelBox = new TextBox(SQ/2, SQ*7, font, SQ, "Level");
-        timeBox = new TextBox(SQ/2, SQ, font, SQ, "Time");
+        TextBox timeBox = new TextBox(SQ/2, SQ, font, SQ, "Time");
         shapeBox = new ShapeBox((SQ*23)/2, SQ, font, SQ, "Next Shape");
 
         scoreBoxes.add(timeBox);
--- ShapeBox.java
+++ ShapeBox.java
@@ -8,15 +8,10 @@ import java.util.*;
 
 
 public class ShapeBox extends ScoreBox {
-	public static void main(String[] args){SwingUtilities.invokeLater(new Runnable() {
-
-            @Override //gets called when the thread is run
-            public void run() {
-                new PentWindow();
-            }});}
+	public static void main(String[] args){}
 
 	public ShapeBox(int x, int y, Font f, int s, String t) {
-        super(x, y, f, s, t, s*3, s*3);
+        super(x, y, f, s, t, s*3, s*4);
     }
 
     public void drawValue(Shape s) {
@@ -30,9 +25,8 @@ public class ShapeBox extends ScoreBox {
         String[][] shape = s.getShape();
 
         //so the shape gets placed in the center of the box.
-        int ix = w/2- (ss*shape.length)/2;
-        
-        int iy = h/2 - (ss*shape[0].length-1)/2 + 5;
+        int ix = (int)(SQ*1.5) - (ss*shape[1].length)/2;
+        int iy = (int)(SQ*1.5) - (ss*shape.length)/2 + ss; 
 
         for (int i=0; i<shape.length; i++) {
             for (int j=0; j<shape[0].length; j++) {
@@ -40,6 +34,7 @@ public class ShapeBox extends ScoreBox {
                     drawBlock(g, i*ss+ix, j*ss+iy, pentColors[0], ss);
             }
         }
+
         repaint();
         g.dispose();
     }
