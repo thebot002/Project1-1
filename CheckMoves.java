dac21ed6b56cfa9b6f6ba9c85433c591e7f73d85
Babis <mpampis.laz@gmail.com>
2017-12-12 12:55:09 +0100

    Improvements for the bot

--- CheckMoves.java
+++ CheckMoves.java
@@ -2,7 +2,7 @@
 import java.util.ArrayList;
 
 public class CheckMoves {
-
+	
 	private PentrisBoard board;
 	private Shape shape;
 	private Shape shape2;
@@ -41,99 +41,72 @@ public class CheckMoves {
 	public void findPossibleMoves() {
 
 		int rot1 = 0;
-		int y=0;
+
+
 		while (rot1 < 4) {
 
 			for (int i = 0; i <= board.getBoard()[0].length - shape.getWidth(); i++) {
-//				while(!board.isPlaced(shape, i, y)) {
-//					y++;
-//				}
-//				board.addShapeToBoard(shape, i, y);
-//				PentrisBoard checkingBoard = new PentrisBoard(board.copyBoard());
-//				Shape checkingShape = new Shape(shape.copyShape());
+				int y=0;
+				while(!board.isPlaced(shape, i, y)) {
+
+					y++;
+				}
+				board.addShapeToBoard(shape,i,y);
+				PentrisBoard checkingBoard = new PentrisBoard(board.copyBoard());
+				Shape checkingShape = new Shape(shape.copyShape());
+				int x=i;
+				findPossibleMoves2(checkingBoard,checkingShape,x);
+
 //				if (board.isPlaced(shape, i, y)) {
 //
 //					boardList.add(checkingBoard);
 //					shapeList.add(checkingShape);
 //					xList.add(i);
-//					board.removeShapeFromBoard(shape, i, y);
+					board.removeShapeFromBoard(shape, i, y);
 //				}
-//			}
-
-
-
-				for (int j = 1; j<=board.getBoard().length - shape.getHeight(); j++) {
-					boolean check=true;
-					for(int k=i; k<shape.getWidth(); k++) {
-
-						int l=0;
-						while(j-l>0 && l<board.getBoard().length) {
-						if(!board.getBoard()[j-l][k].equals("-"))
-							check=false;
-						l++;
-						}
-					}
-					if (board.insertionPossible(shape, i, j) && check) {
-						board.addShapeToBoard(shape, i, j);
-						PentrisBoard checkingBoard = new PentrisBoard(board.copyBoard());
-						Shape checkingShape = new Shape(shape.copyShape());
-						if (board.isPlaced(shape, i, j)) {
-
-							boardList.add(checkingBoard);
-							shapeList.add(checkingShape);
-							xList.add(i);
-							board.removeShapeFromBoard(shape, i, j);
-						} else
-							board.removeShapeFromBoard(shape, i, j);
-					}
-				}
+				y=0;
 			}
-
 
 			shape.rotateR();
 			rot1++;
 		}
 
-		for(int i=0; i<xList.size(); i++) {
-			System.out.println("xLIST "+i);
-			System.out.println(xList.get(i));
-			System.out.println("BOARD LIST "+i);
-			boardList.get(i).printBoard();
-			System.out.println("SHAPE LIST "+i);
-			shapeList.get(i).printShape();
-			System.out.println("");
-		}
+
 	}
 
 
-//		public void findPossibleMoves2() {
-//
-//			int rot2 = 0;
-//			while (rot2 < 4) {
-//				for (int i = 0; i <= board.getBoard()[0].length - shape.getWidth(); i++) {
-//					for (int j = 0; j<=board.getBoard().length - shape.getHeight(); j++) {
-//						if (board.insertionPossible(shape2, i, j)) {
-//							board.addShapeToBoard(shape2, i, j);
-//							PentrisBoard checkingBoard2 = new PentrisBoard(board.copyBoard());
-//							Shape checkingShape2 = new Shape(shape2.copyShape());
-//							if (board.isPlaced(shape2, i, j)) {
-//
-//								boardList.add(checkingBoard2);
-//								shapeList2.add(checkingShape2);
-//								xList2.add(i);
-//								board.removeShapeFromBoard(shape2, i, j);
-//							} else
-//								board.removeShapeFromBoard(shape2, i, j);
-//						}
-//					}
-//				}
-//
-//				shape2.rotateR();
-//				rot2++;
-//			}
+public void findPossibleMoves2(PentrisBoard checkingBoard, Shape checkingShape, int x) {
 
+		int rot2 = 0;
 
-//	}
+		while (rot2 < 4) {
+
+			for (int i = 0; i <= checkingBoard.getBoard()[0].length - shape2.getWidth(); i++) {
+
+				int y=0;
+				while(!checkingBoard.isPlaced(shape2, i, y)) {
+					y++;
+				}
+				checkingBoard.addShapeToBoard(shape2,i,y);
+				PentrisBoard checkingBoard2 = new PentrisBoard(checkingBoard.copyBoard());
+				Shape checkingShape2 = new Shape(checkingShape.copyShape());
+				if (checkingBoard.isPlaced(shape2, i, y)) {
+
+					boardList.add(checkingBoard2);
+					shapeList.add(checkingShape);
+					xList.add(x);
+
+				}
+				checkingBoard.removeShapeFromBoard(shape2, i, y);
+				y=0;
+			}
+
+			shape2.rotateR();
+			rot2++;
+		}
+
+//
+	}
 
 
 }
--- GameCanvas.java
+++ GameCanvas.java
@@ -11,168 +11,284 @@ import javax.swing.Timer;
 class GameCanvas extends PentPanel implements ActionListener {
 	public static void main(String[] args) {}
 
+	private ShapeBox shapeBox;
+	private TimeBox timeBox;
+	private TextBox scoreBox;
+	private Timer timer;
+	private ArrayList<ScoreBox> scoreBoxes = new ArrayList<ScoreBox>();
 
-    private Timer timer;
-    private ArrayList<ScoreBox> scoreBoxes= new ArrayList<ScoreBox>();
-    public Color[][] pentColors = {
-        {new Color(255,255,127), new Color(255,255,0), new Color(127,127,0)},   //yellow
-        {new Color(255,127,255), new Color(255,0,255), new Color(127,0,127)},   //purple
-        {new Color(127,255,255), new Color(0,255,255), new Color(0,127,127)}    //cyan
-    }; 
+	private Random random = new Random();
+	private int time = 0;
 
-    public int[][] board = {
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,1,1,0},
-        {0,0,1,0,0},
-        {0,0,1,0,0},
-        {0,0,1,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-    };
+	public PentrisBoard board = new PentrisBoard();
 
+	private Shape activeShape;
+	private Shape nextShape;
+	private ShapeList shapeList;
+	private Timer runtime;
+
+	private int speedDefault = 200;
+	private int speedUp = 100;
+	//shape x and y:
+	private int x=0;
+	private int y=0;
+	private int score = 0;
+	private boolean gameRunning = false;
+	private PentrisBoard checkBoard;
+	private Shape checkShape;
+	private Shape checkShape2;
 	public GameCanvas(int W, int H,  Font f, int s) {
-        super(W, H, f, s, 0, 0);
-        drawGame();
-        startGame();
-    }
+		super(0, 0, W, H, f, s);
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawImage(image, 0, 0, null);
-    }
+		shapeList = new ShapeList();
+		activeShape = shapeList.getRandomShape();
+		nextShape = shapeList.getRandomShape();
 
-    private void tick() {
-        drawScoreBoxes();
-    }
+		drawGame();
+		startGame();
+	}
 
-    public void startGame() {
-        drawBoard(board);
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(image, 0, 0, null);
+	}
 
-        timer = new Timer(1000, this);
-        timer.start();
-    }
-    
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        tick();
-        repaint();
-    }
+	public void startGame() {
+		gameRunning = true;
 
-    public void upKeyPress() {
-        //rotateShape();
-    }
 
-    public void downKeyPress() {
-        //speedUp();
-    }
 
-    public void spaceKeyPress() {
-        //dropDown();
-    }
+		if(activeShape.getHeight()>activeShape.getWidth()) activeShape.rotateR();
+		checkBoard = new PentrisBoard(board.copyBoard());
+		checkShape = new Shape(activeShape.copyShape());
+		checkShape2= new Shape(nextShape.copyShape());
+		if(!board.addShapeToBoard(activeShape)) gameOver();
+		drawBoard(board);
+
+		timer = new Timer(speedDefault, this);
+	   	timer.start();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		CheckMoves psblMoves = new CheckMoves(checkBoard,checkShape,checkShape2);
+		psblMoves.findPossibleMoves();
+
+//		for(int i=0; i<psblMoves.getXList().size(); i++) {System.out.println(psblMoves.getXList());}
+		ArrayList<Integer> checkXList = new ArrayList<Integer>();
+		ArrayList<PentrisBoard> checkBoardList = new ArrayList<PentrisBoard>();
+		ArrayList<Shape> checkShapeList = new ArrayList<Shape>();
+		for(int i=0; i<psblMoves.getXList().size(); i++) {
+			checkXList.add(psblMoves.getXList().get(i));
+			checkBoardList.add(psblMoves.getBoardList().get(i));
+			checkShapeList.add(psblMoves.getShapeList().get(i));
+		}
+
+
+
+
+		FindBestFit bestFit = new FindBestFit(checkBoardList, checkShapeList, checkXList);
+		bestFit.findOptimalState();
+
+
+			playingBot(bestFit.getBestShape(), bestFit.getOptimalX());
+
+
+		//stops tick events that were created before the game ended trying to do things after the game ends.
+		if(gameRunning && !paused) {
+			//shape is touching another shape
+			if(board.isPlaced(activeShape,x,y)) {
+				if(y<5) {
+					gameOver();
+					psblMoves.emptyArrayList();
+				} else {
+					checkBoard=new PentrisBoard(board.copyBoard());
+					psblMoves.emptyArrayList();
+					activeShape = nextShape;
+					checkShape = new Shape(activeShape.copyShape());
+					nextShape = shapeList.getRandomShape();
+					checkShape2 = new Shape(nextShape.copyShape());
+					shapeBox.drawValue(nextShape);
+					if(activeShape.getHeight() > activeShape.getWidth()) activeShape.rotateR();
+					x=0;
+					y=0;
+					while(y+activeShape.getHeight()<5) {
+						y++;
+					}
+				}
+				int lines = board.breakLines();
+				score += lines * lines * 10;
+
+				scoreBox.setTarget(score);
+
+			} else {
+				board.moveDown(activeShape,x,y);
+				y++;
+			}
+			drawBoard(board);
+			repaint();
+		}
+	}
+
+	public void playingBot(Shape bestShape, int optimalX) {
+		int br1 = 0;
+		int br2 = 0;
+		int br3 = 0;
+
+		while(!activeShape.equals(bestShape) && br1<10) {
+			upKeyPress();
+			br1++;
+		}
+		while (optimalX<x && br2<10) {
+			leftKeyPress();
+			br2++;
+		}
+		while(optimalX>x && br3<10) {
+			rightKeyPress();
+			br3++;
+		}
+//		if(optimalX==x && activeShape.equals(bestShape))
+//		downKeyPress();
+	}
+
+	public Boolean isRunning() {
+		return gameRunning;
+	}
+
+	public void upKeyPress() {
+		  if(gameRunning && !paused && board.rotatePossible(activeShape,x,y))
+			board.removeShapeFromBoard(activeShape, x, y);
+		    activeShape.rotateR();
+		    board.addShapeToBoard(activeShape,x,y);
+		  drawBoard(board);
+	}
+
+	public void downKeyPress() {
+		if(!paused)
+			timer.setDelay(speedUp/2);
+	}
+
+	public void downKeyRelease() {
+		if(!paused)
+			timer.setDelay(speedDefault);
+	}
+
+	public void spaceKeyPress() {
+		if(!paused)	{
+			board.dropDown(activeShape, x, y);
+		  	activeShape = nextShape;
+		  	checkShape = new Shape(activeShape.copyShape());
+		  	nextShape = shapeList.getRandomShape();
+		  	if(activeShape.getHeight()>activeShape.getWidth()) activeShape.rotateR();
+		  	x=0;
+		  	y=0;
+		  	score += board.breakLines();
+		  	board.addShapeToBoard(activeShape,x,y);
+		  	checkBoard=new PentrisBoard(board.copyBoard());
+		  	drawBoard(board);
+		}
+	}
 
     public void leftKeyPress() {
-        //moveLeft();
+         if(gameRunning && !paused && board.moveLeftPossible(activeShape, x, y)) {
+		  		board.moveLeft(activeShape,x,y);
+		  		x--;
+			}
+		   drawBoard(board);
     }
+
     public void rightKeyPress() {
-        //moveRight();
-    }
+		 	if(gameRunning && !paused && board.moveRightPossible(activeShape, x, y)) {
+        		board.moveRight(activeShape,x,y);
+		  		x++;
+	  		}
+		   drawBoard(board);
+	}
 
-    private void drawGame() {
-        Graphics g = image.getGraphics();
-        g.setColor(BACKGROUND);
-        g.fillRect(0,0,w,h);
+	public void pKeyPress() {
+		paused = !paused;
+	}
 
-
-        //'origin' x and y positions to draw the board from.
-        int ox = (w-SQ*5)/2;
-        int oy = SQ;
-    
-
-        //draw border
-        for(int y=-1; y<16; y++) {
-            drawBlock(g, ox - SQ, oy + SQ * y, pentColors[0]);
-            drawBlock(g, ox + 5 * SQ, oy + SQ * y, pentColors[0]);
-        }
-
-        for(int x=0; x<5; x++) {
-            drawBlock(g, ox + SQ * x, oy - SQ, pentColors[0]);
-            drawBlock(g, ox + SQ * x, oy + 15 * SQ, pentColors[0]);
-        }
+	private void drawGame() {
+		Graphics g = image.getGraphics();
+		g.setColor(BACKGROUND);
+		g.fillRect(0,0,w,h);
 
 
-        //create score boxes
-        TextBox scoreBox = new TextBox(SQ/2, SQ*4, font, SQ, "Score");
-        TextBox highScoreBox = new TextBox((SQ*23)/2, SQ*5, font, SQ, "High Score");
-        TextBox levelBox = new TextBox(SQ/2, SQ*7, font, SQ, "Level");
-        TextBox timeBox = new TextBox(SQ/2, SQ, font, SQ, "Time");
-        ShapeBox shapeBox = new ShapeBox((SQ*23)/2, SQ, font, SQ, "Next Shape");
+		//'origin' x and y positions to draw the board from.
+		int ox = (w-SQ*5)/2;
+		int oy = SQ;
 
-        scoreBoxes.add(timeBox);
-        scoreBoxes.add(shapeBox);
-        scoreBoxes.add(levelBox);
-        scoreBoxes.add(highScoreBox);
-        scoreBoxes.add(scoreBox);
+		//draw border of board
+		for(int y=-1; y<16; y++) {
+			drawBlock(g, ox - SQ, oy + SQ * y, BACKGROUND, SQ);
+			drawBlock(g, ox + 5 * SQ, oy + SQ * y, BACKGROUND, SQ);
+		}
 
-        for(ScoreBox box : scoreBoxes) {
-            add(box);
-        }
-
-        repaint();
-    }
-
-    private void drawScoreBoxes() {
-        for(ScoreBox box : scoreBoxes) {
-            box.tick();
-        }
-    } 
-
-    private void drawBoard(int[][] board) {
-
-        int ox = (w-SQ*5)/2;
-        int oy = SQ;
-
-        Graphics g = image.getGraphics();
-        g.setColor(BACKGROUND);
-        g.fillRect(ox,oy,SQ*5,SQ*15);
+		for(int x=0; x<5; x++) {
+			drawBlock(g, ox + SQ * x, oy-SQ, BACKGROUND, SQ);
+			drawBlock(g, ox + SQ * x, oy + 15 * SQ, BACKGROUND, SQ);
+		}
 
 
-        for(int x=0; x<board[0].length; x++) {
-            for(int y=0; y<board.length; y++) {
-                if(board[y][x] != 0)
-                    drawBlock(g, ox + x * SQ, oy + y * SQ, pentColors[board[y][x]]);
-            }
-        }
+		//create score boxes
+		TextBox highScoreBox = new TextBox((SQ*23)/2, SQ*6, font, SQ, "High Score", this);
+		//TextBox levelBox = new TextBox(SQ/2, SQ*7, font, SQ, "Level", this);
+		scoreBox = new TextBox(SQ/2, SQ*4, font, SQ, "Score", this);
+		timeBox = new TimeBox(SQ/2, SQ, font, SQ, "Time", this);
+		shapeBox = new ShapeBox((SQ*23)/2, SQ, font, SQ, "Next Shape", nextShape, this);
 
-        repaint();
-    }
+		scoreBoxes.add(scoreBox);
+		//scoreBoxes.add(levelBox);
+		scoreBoxes.add(highScoreBox);
 
-    private void drawBlock(Graphics g, int x, int y, Color[] c) {
-        int[] xp = {x, x, x + SQ};
-        int[] yp = {y, y + SQ, y};
+		add(highScoreBox);
+		//add(levelBox);
+		add(scoreBox);
+		add(timeBox);
+		add(shapeBox);
 
-        g.setColor(c[0]);
-        g.fillPolygon(new Polygon(xp, yp, 3));
+		if(scoreManager.doesHighScoresExist())
+			highScoreBox.setValue(scoreManager.getMaxScore().getScore());
+
+		repaint();
+	}
+
+	private void drawBoard(PentrisBoard pBoard) {
+
+		int ox = (w-SQ*5)/2;
+		int oy = -4*SQ;
+
+		Graphics g = image.getGraphics();
+		g.setColor(BACKGROUND);
+		g.fillRect(ox,oy+5*SQ,SQ*5,SQ*15);
+
+		String[][] board = pBoard.getBoard();
 
 
-        xp[0] = x + SQ;
-        yp[0] = y + SQ;
+		for(int x=0; x<board[0].length; x++) {
+			for(int y=5; y<board.length; y++) {
+				if(!board[y][x].equals("-")) {
+					drawBlock(g, ox + x * SQ, oy + y * SQ, colorList.get(board[y][x]), SQ);
+				}
+			}
+		}
 
-        g.setColor(c[2]);
-        g.fillPolygon(new Polygon(xp, yp, 3));
+		repaint();
+	}
 
-        int inner = (int)(SQ * 7)/10;
-        int gap = (int)(SQ-inner)/2;
+	public int getScore() {
+		return score;
+	}
 
-        g.setColor(c[1]);
-        g.fillRect(x + gap, y + gap, inner, inner);
-    }
+	private void gameOver() {
+	 	gameRunning = false;
+		activeShape = null;
+		timer.stop();
+		String name = JOptionPane.showInputDialog(this,"Enter Your Name: ");
+		PentWindow p = (PentWindow)SwingUtilities.getRoot(this);
+		p.endGame(name, getScore());
+	}
 }
