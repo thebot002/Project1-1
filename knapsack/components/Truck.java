fdfcc7688829ff7b479f7bcf224064f7e4a205f0
thebot002 <31898693+thebot002@users.noreply.github.com>
2018-01-09 15:49:24 +0100

    JavaDoc added

--- knapsack/components/Truck.java
+++ knapsack/components/Truck.java
@@ -4,6 +4,9 @@ import javafx.geometry.Point3D;
 
 import java.util.*;
 
+/**
+ * Class used to define the truck where the parcels or pentominoes are going to be placed.
+ */
 public class Truck {
 	public ArrayList<Parcel> parcelList;
 	private int length;
@@ -11,7 +14,14 @@ public class Truck {
 	private int width;
 	private String[][][] truck;
 
-	//Create a parcel with a default position set to the origin and set its 8 vertices.
+	/**
+	 *Create a parcel with a default position set to the origin and set its 8 vertices.
+     *Object constructor.
+     *
+     * @param l The length of the Truck.
+     * @param h The height of the Truck.
+     * @param w The width of the Truck
+	 */
 	public Truck(double l, double h, double w) {
 	    parcelList = new ArrayList<>();
 		length = (int) (l/0.5);
@@ -27,14 +37,35 @@ public class Truck {
         }
 	}
 
+    /**
+     * Default constructor for this project. Truck with size: 16.5 x 4.0 x 2.5 .
+     */
 	public Truck(){
 	    this(2.5,4.0,16.5);
     }
 
+    /**
+     * Method to get the length of the truck. (z axis)
+     * @return The length of the truck.
+     */
 	public int getLength() { return length; }
-	public int getHeight() { return height; }
-	public int getWidth()  { return width;  }
+    /**
+     * Method to get the height of the truck. (y axis)
+     * @return The height of the truck.
+     */
 
+	public int getWidth()  { return width;  }
+    /**
+     * Method to get the width of the truck. (x axis)
+     * @return The width of the truck.
+     */
+    public int getHeight() { return height; }
+
+    /**
+     * Method to add a parcel object to the truck.
+     * @param p The parcel to be added to the truck.
+     * @param pos The position where the parcel will be added.
+     */
     public void addParcel(Parcel p, Point3D pos){
 	    parcelList.add(p);
 	    int x = (int) (pos.getX()/0.5);
@@ -49,6 +80,10 @@ public class Truck {
         }
 	}
 
+    /**
+     * Method to remove a parcel from the truck.
+     * @param p The parcel to be removed.
+     */
 	public void removeParcel(Parcel p){
 	    parcelList.remove(p);
         Point3D pos = p.getPos();
