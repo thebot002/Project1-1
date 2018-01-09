cd75e642c440095023cad0137bf12a6b161d59a9
Takowski <takowski@outlook.com>
2018-01-09 12:41:25 +0100

    Added value to the constructor
    And made it so the default value is 1 when constructing

--- knapsack/frame/Parcel.java
+++ knapsack/frame/Parcel.java
@@ -1,28 +1,29 @@
 package knapsack.frame;
 
-import javafx.geometry.Point3D; 
+import javafx.geometry.Point3D;
 import java.util.*;
 
-public class Parcel {
-	public ArrayList<Point3D> points;
-	private double length;
-	private double height;
-	private double width;
-	private Point3D pos;
-	private int value = 1;
+	public class Parcel {
+		public ArrayList<Point3D> points;
+		private double length;
+		private double height;
+		private double width;
+		private Point3D pos;
+		private double value;
 
-	//Create a parcel with a default position set to the origin and set its 8 vertices.
-	public Parcel(double l, double h, double w) {
+	//Create a parcel with a default position set to the origin and set its 8 vertices and a value.
+	public Parcel(double l, double h, double w, double val) {
 		length = l;
 		height = h;
 		width = w;
-
+		value = val;
+		
 		pos = new Point3D(0,0,0);
 
 		points = new ArrayList<Point3D>();
 		points.add(new Point3D(0, 0, 0));
 		points.add(new Point3D(0, h, 0));
-		points.add(new Point3D(l, h, 0)); 
+		points.add(new Point3D(l, h, 0));
 		points.add(new Point3D(l, 0, 0));
 
 		points.add(new Point3D(0, 0, w));
@@ -31,7 +32,11 @@ public class Parcel {
 		points.add(new Point3D(l, 0, w));
 	}
 
-	public void setValue(int value) { this.value = value; }
+	public Parcel(double l, double h, double w) {
+		this(double l, double h, double w, 1)
+	}
+
+	public void setValue(double value) { this.value = value; }
 
 	public void setPos(Point3D pos) { this.pos = pos; }
 
