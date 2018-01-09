06dcd6edddba2b3ea42c4381ff04a2cb528e938c
Takowski <takowski@outlook.com>
2018-01-09 13:33:19 +0100

    Commented decimal RGB colors

--- knapsack/frame/ShapeColors.java
+++ knapsack/frame/ShapeColors.java
@@ -6,28 +6,27 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ShapeColors extends ArrayList<Color> {
 	public ShapeColors() {
-		add(new Color(255, 102, 255));
-		add(new Color(0  , 255, 255));
+		add(new Color(255, 102, 255)); //pink
+		add(new Color(0  , 255, 255)); //cyan
 		add(Color.BLUE);
-		add(new Color(255, 79 , 0  ));
-		add(new Color(255, 178, 102));
-		add(new Color(0  , 204, 0  ));
-		add(new Color(102, 255, 102));
+		add(new Color(255, 79 , 0  )); //orange
+		add(new Color(218, 165, 32)); //goldenrod
+		add(new Color(0  , 204, 0  )); //free speech green
+		add(new Color(102, 255, 102)); //lime
 		add(Color.RED);
-		add(new Color(255, 102, 102));
-		add(new Color(137, 107, 255));
-		add(new Color(218, 165, 32 ));
-		add(new Color(0  , 102, 0  ));
-		add(new Color(153, 0  , 153));
-		add(new Color(127, 35, 17  ));
+		add(new Color(255, 102, 102)); //bittersweet
+		add(new Color(137, 107, 255)); //light slate blue
+		add(new Color(236, 209, 141 )); //double colonial white
+		add(new Color(0  , 102, 0  )); //green
+		add(new Color(153, 0  , 153)); //dark magenta
+		add(new Color(127, 35, 17  )); //falu red
 		add(Color.YELLOW);
-		add(new Color(255, 255, 153));
-		add(new Color(128, 128, 128)); 
+		add(new Color(255, 255, 153)); //canary
+		add(new Color(128, 128, 128)); //gray
 	}
 
 	public Color getRand() {
 		return get(ThreadLocalRandom.current().nextInt(0, size()));
-		
+
 	}
 }
-	
