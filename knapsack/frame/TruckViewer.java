3579371830dc6265bb3ad538595eb2ee80d7fd6c
thebot002 <31898693+thebot002@users.noreply.github.com>
2018-01-10 14:54:02 +0100

    Revert "Projection gui"

--- knapsack/frame/TruckViewer.java
+++ knapsack/frame/TruckViewer.java
@@ -1,13 +1,23 @@
 package knapsack.frame;
 
+import knapsack.frame.*;
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.border.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
 import javax.swing.JPanel;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.io.*;
+import java.awt.FlowLayout;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class TruckViewer extends JFrame {
 
@@ -17,7 +27,6 @@ public class TruckViewer extends JFrame {
     private Font font;
 
     private CubeDrawer c;
-    private Menu m;
 
     public TruckViewer() {
         SwingUtilities.invokeLater(new Runnable() {
@@ -26,7 +35,7 @@ public class TruckViewer extends JFrame {
                 create();
             }
         });
-    }
+    }        
 
     private void create() {
         setSize(W, H);
@@ -34,46 +43,17 @@ public class TruckViewer extends JFrame {
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        m = new Menu();
-        m.setPreferredSize(new Dimension(W/4*1, H));
-        m.setVisible(true);
+        JPanel p = new JPanel();
+        p.setPreferredSize(new Dimension(W, 300));
 
-        c = new CubeDrawer(W, H);
-        c.setPreferredSize(new Dimension(W/4*3, H));
+        c = new CubeDrawer(W, 400);
+        c.setPreferredSize(new Dimension(W, 400));
         c.setVisible(true);
 
-        JPanel container = new JPanel();
-        container.setLayout(new BoxLayout(container, BoxLayout.X_AXIS));
-        container.add(c);
-        container.add(m);
-        add(container);
+        add(c);
+        //add(p);
         pack();
         setVisible(true);
-        addKeyInput();
-    }
-
-    public void addKeyInput() {
-        this.addKeyListener( new KeyAdapter() {    //Key listener
-            public void keyPressed (KeyEvent e){
-                int key = e.getKeyCode();
-                //System.out.println(key);
-
-                if (key == 40) { //down arrow
-                    c.rollDown();
-                }
-
-                if (key == 38) { //up arrow
-                    c.rollUp();
-                }
-
-                if (key == 39) { //right arrow
-                    c.rotateRight();
-                }
-
-                if (key == 37) { //left arrow
-                    c.rotateLeft();
-                }
-            }
-        });
     }
 }
+    
