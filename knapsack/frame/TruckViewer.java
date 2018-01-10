ea9c161f42ca1f3d9ea2f8382d17ef8e8dc898f9
nicxcin <nicholas.beaumont21@gmail.com>
2018-01-10 13:45:28 +0100

    Complete Version of Projection Rendering

--- knapsack/frame/TruckViewer.java
+++ knapsack/frame/TruckViewer.java
@@ -1,23 +1,13 @@
 package knapsack.frame;
 
-import knapsack.frame.*;
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.border.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.Toolkit;
 import javax.swing.JPanel;
-import java.io.*;
-import java.awt.FlowLayout;
-import java.util.concurrent.ThreadLocalRandom;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class TruckViewer extends JFrame {
 
@@ -44,16 +34,42 @@ public class TruckViewer extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         JPanel p = new JPanel();
-        p.setPreferredSize(new Dimension(W, 300));
+        p.setPreferredSize(new Dimension(W, H));
 
-        c = new CubeDrawer(W, 400);
-        c.setPreferredSize(new Dimension(W, 400));
+        c = new CubeDrawer(W, H);
+        c.setPreferredSize(new Dimension(W, H));
         c.setVisible(true);
 
         add(c);
         //add(p);
         pack();
         setVisible(true);
+        addKeyInput();
+    }
+
+    public void addKeyInput() {
+        this.addKeyListener( new KeyAdapter() {    //Key listener
+            public void keyPressed (KeyEvent e){
+                int key = e.getKeyCode();
+                //System.out.println(key);
+
+                if (key == 40) { //down arrow
+                    c.rollDown();
+                }
+
+                if (key == 38) { //up arrow
+                    c.rollUp();
+                }
+
+                if (key == 39) { //right arrow
+                    c.rotateRight();
+                }
+
+                if (key == 37) { //left arrow
+                    c.rotateLeft();
+                }
+            }
+        });
     }
 }
     
