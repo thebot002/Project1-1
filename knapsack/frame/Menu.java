649e139f05748e35ad0dafe3cfd5bd9c6da6764b
Takowski <takowski@outlook.com>
2018-01-12 17:30:23 +0100

    Updated menu functionality

--- knapsack/frame/Menu.java
+++ knapsack/frame/Menu.java
@@ -1,6 +1,5 @@
 package knapsack.frame;
 
-import knapsack.*;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.GridBagLayout;
@@ -8,13 +7,25 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JTabbedPane;
 import javax.swing.JLabel;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.LineBorder;
-import java.awt.Color;
+import javax.swing.JOptionPane;
 import javax.swing.border.EtchedBorder;
 import java.awt.SystemColor;
+import java.text.ParseException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Menu extends JPanel {
+public class Menu extends JPanel implements ActionListener {
+	
+	private JButton btnFillTruck;
+	private JButton btnClearTruck;
+	private JButton btnResetCamera;
+	private JLabel lblGapsFound;
+	private JLabel lblCurrentValue;
+	private JLabel lblGapsFoundDisp;
+	private JLabel lblCurrentValueDisp;
+	private JTabbedPane tabbedPane;
+	private SwitchTabbedPane rectangleTab;
+	private SwitchTabbedPane pentominoTab;
 
 	public Menu() {
 		setBackground(SystemColor.menu);
@@ -26,7 +37,8 @@ public class Menu extends JPanel {
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 
-		JButton btnFillTruck = new JButton("Fill Truck");
+		btnFillTruck = new JButton("Fill Truck");
+		btnFillTruck.addActionListener(this);
 		GridBagConstraints gbc_btnFillTruck = new GridBagConstraints();
 		gbc_btnFillTruck.gridwidth = 2;
 		gbc_btnFillTruck.insets = new Insets(0, 0, 5, 5);
@@ -34,7 +46,7 @@ public class Menu extends JPanel {
 		gbc_btnFillTruck.gridy = 1;
 		add(btnFillTruck, gbc_btnFillTruck);
 
-		JButton btnClearTruck = new JButton("Clear Truck");
+		btnClearTruck = new JButton("Clear Truck");
 		GridBagConstraints gbc_btnClearTruck = new GridBagConstraints();
 		gbc_btnClearTruck.gridwidth = 2;
 		gbc_btnClearTruck.insets = new Insets(0, 0, 5, 5);
@@ -42,15 +54,47 @@ public class Menu extends JPanel {
 		gbc_btnClearTruck.gridy = 1;
 		add(btnClearTruck, gbc_btnClearTruck);
 
-		JButton btnresetCamera = new JButton("Reset Camera");
-		GridBagConstraints gbc_btnresetCamera = new GridBagConstraints();
-		gbc_btnresetCamera.gridwidth = 2;
-		gbc_btnresetCamera.insets = new Insets(0, 0, 5, 5);
-		gbc_btnresetCamera.gridx = 5;
-		gbc_btnresetCamera.gridy = 1;
-		add(btnresetCamera, gbc_btnresetCamera);
+		btnResetCamera = new JButton("Reset Camera");
+		GridBagConstraints gbc_btnResetCamera = new GridBagConstraints();
+		gbc_btnResetCamera.gridwidth = 2;
+		gbc_btnResetCamera.insets = new Insets(0, 0, 5, 5);
+		gbc_btnResetCamera.gridx = 5;
+		gbc_btnResetCamera.gridy = 1;
+		add(btnResetCamera, gbc_btnResetCamera);
 
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		lblGapsFound = new JLabel("Gaps Found");
+		GridBagConstraints gbc_lblGapsFound = new GridBagConstraints();
+		gbc_lblGapsFound.gridwidth = 2;
+		gbc_lblGapsFound.insets = new Insets(0, 0, 5, 5);
+		gbc_lblGapsFound.gridx = 2;
+		gbc_lblGapsFound.gridy = 11;
+		add(lblGapsFound, gbc_lblGapsFound);
+
+		lblCurrentValue = new JLabel("Current Value");
+		GridBagConstraints gbc_lblCurrentValue = new GridBagConstraints();
+		gbc_lblCurrentValue.gridwidth = 2;
+		gbc_lblCurrentValue.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCurrentValue.gridx = 4;
+		gbc_lblCurrentValue.gridy = 11;
+		add(lblCurrentValue, gbc_lblCurrentValue);
+
+		lblGapsFoundDisp = new JLabel("---");
+		GridBagConstraints gbc_lblGapsFoundDisp = new GridBagConstraints();
+		gbc_lblGapsFoundDisp.gridwidth = 2;
+		gbc_lblGapsFoundDisp.insets = new Insets(0, 0, 5, 5);
+		gbc_lblGapsFoundDisp.gridx = 2;
+		gbc_lblGapsFoundDisp.gridy = 12;
+		add(lblGapsFoundDisp, gbc_lblGapsFoundDisp);
+
+		lblCurrentValueDisp = new JLabel("---");
+		GridBagConstraints gbc_lblCurrentValueDisp = new GridBagConstraints();
+		gbc_lblCurrentValueDisp.gridwidth = 2;
+		gbc_lblCurrentValueDisp.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCurrentValueDisp.gridx = 4;
+		gbc_lblCurrentValueDisp.gridy = 12;
+		add(lblCurrentValueDisp, gbc_lblCurrentValueDisp);
+		
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		GridBagConstraints gbc_tabbedPane = new GridBagConstraints();
 		gbc_tabbedPane.gridheight = 7;
 		gbc_tabbedPane.gridwidth = 6;
@@ -60,43 +104,50 @@ public class Menu extends JPanel {
 		gbc_tabbedPane.gridy = 3;
 		add(tabbedPane, gbc_tabbedPane);
 
-		BaseTabbedPane rectangleTab = new BaseTabbedPane("A", "B", "C", "Dimensions", "1.0 x 1.0 x 2.0", "1.0 x 1.5 x 2.0", "1.5 x 1.5 x 1.5");
-		rectangleTab.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		rectangleTab.setBackground(SystemColor.menu);
-		BaseTabbedPane pentominoTab = new BaseTabbedPane("L", "P", "T", "", "", "", "");
+		SwitchTabbedPane rectangleTab = new SwitchTabbedPane("A", "B", "C", "Dimensions", "1.0 x 1.0 x 2.0", "1.0 x 1.5 x 2.0", "1.5 x 1.5 x 1.5");
+		SwitchTabbedPane pentominoTab = new SwitchTabbedPane("L", "P", "T", "", "", "", "");
 		tabbedPane.addTab("Rectangle", rectangleTab);
 		tabbedPane.addTab("Pentomino", pentominoTab);
-
-		JLabel lblGapsFound = new JLabel("Gaps Found");
-		GridBagConstraints gbc_lblGapsFound = new GridBagConstraints();
-		gbc_lblGapsFound.gridwidth = 2;
-		gbc_lblGapsFound.insets = new Insets(0, 0, 5, 5);
-		gbc_lblGapsFound.gridx = 2;
-		gbc_lblGapsFound.gridy = 11;
-		add(lblGapsFound, gbc_lblGapsFound);
-
-		JLabel lblCurrentValue = new JLabel("Current Value");
-		GridBagConstraints gbc_lblCurrentValue = new GridBagConstraints();
-		gbc_lblCurrentValue.gridwidth = 2;
-		gbc_lblCurrentValue.insets = new Insets(0, 0, 5, 5);
-		gbc_lblCurrentValue.gridx = 4;
-		gbc_lblCurrentValue.gridy = 11;
-		add(lblCurrentValue, gbc_lblCurrentValue);
-
-		JLabel gapsFound = new JLabel("---");
-		GridBagConstraints gbc_gapsFound = new GridBagConstraints();
-		gbc_gapsFound.gridwidth = 2;
-		gbc_gapsFound.insets = new Insets(0, 0, 5, 5);
-		gbc_gapsFound.gridx = 2;
-		gbc_gapsFound.gridy = 12;
-		add(gapsFound, gbc_gapsFound);
-
-		JLabel currentValue = new JLabel("---");
-		GridBagConstraints gbc_currentValue = new GridBagConstraints();
-		gbc_currentValue.gridwidth = 2;
-		gbc_currentValue.insets = new Insets(0, 0, 5, 5);
-		gbc_currentValue.gridx = 4;
-		gbc_currentValue.gridy = 12;
-		add(currentValue, gbc_currentValue);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == btnFillTruck) {
+			if(tabbedPane.getSelectedComponent() == rectangleTab) {
+	        	try {
+					rectangleTab.collectParcels();
+					int[][] parcelsRectangle = rectangleTab.collectParcels();
+					
+				} catch (ParseException e1) {
+					System.out.println("The currently edited value couldn't be commited.");
+					JOptionPane.showMessageDialog(tabbedPane, "The currently edited value couldn't be commited.");
+				}
+	        }
+	        else if(tabbedPane.getSelectedComponent() == pentominoTab) {
+	        	try {
+					int[][] parcelsPentomino = pentominoTab.collectParcels();
+					
+				} catch (ParseException e1) {
+					System.out.println("The currently edited value couldn't be commited.");
+					JOptionPane.showMessageDialog(tabbedPane, "The currently edited value couldn't be commited.");
+				}
+	        }
+	        else {
+	        	JOptionPane.showMessageDialog(tabbedPane, "Please switch to the tab that corresponds to the type of parcel you would want to fill the truck with (Rectangle or Pentomino).");     	
+	        }
+		}
+		else if(e.getSource() == btnClearTruck) {
+			
+		}
+		else if(e.getSource() == btnResetCamera) {
+			
+		}
+	}
+	
+	public void setGapsFound(int found) {
+		lblGapsFoundDisp.setText(String.valueOf(found));
+	}
+	
+	public void setCurrentValue(int value) {
+		lblGapsFoundDisp.setText(String.valueOf(value));
 	}
 }
