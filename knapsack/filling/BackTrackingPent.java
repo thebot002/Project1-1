85953a129afff4c22f3f2cc68b492d7cf8907575
Takowski <takowski@outlook.com>
2018-01-23 23:32:41 +0100

    my head is dying

--- knapsack/filling/BackTrackingPent.java
+++ knapsack/filling/BackTrackingPent.java
@@ -391,6 +391,7 @@ public class BackTrackingPent {
 			/* If the present parcel fits to the empty spot. We continue from here. */
 			if(truck.isPossible(parcelAr[index], parcelAr[index].getArray(), p	)) {
 				p = new Point3D(position[0],position[1],position[2]);
+				newTruck.addParcel(parcelArr[index], parcelArr[index].getArray(), p);
 				Truck newTruck = new Truck();
 				newTruck.setTruck(truck.copyTruck());
 
