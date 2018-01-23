ca73c4d3477f4739a28cd5d3740a3b6be01df994
Babis <mpampis.laz@gmail.com>
2018-01-23 22:32:00 +0100

    just commented

--- knapsack/filling/BackTrackingPent.java
+++ knapsack/filling/BackTrackingPent.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import javafx.geometry.Point3D;
 import knapsack.components.Parcel;
 import knapsack.components.PentominoParcel;
-import knapsack.components.Simulations;
 import knapsack.components.Truck;
 
 public class BackTrackingPent {
